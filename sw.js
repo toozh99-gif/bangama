6acb1ffcf1cdf5ad101b410cbe3909e753a7dc4b
prometheus_jid <toozh99@gmail.com>
2026-02-09 05:35:22 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,7 @@
-// Service Worker for Nano Banana Pro
-const CACHE_NAME = 'nano-banana-v1';
-const urlsToCache = [
-  '/',
-  '/nano-banana-pro.html',
-  '/manifest.json'
-];
-
-// Install event
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (e) => {
+  console.log('Prometheus System: Offline Mode Active');
 });
 
-// Fetch event
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
-
-// Activate event
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request));
 });
