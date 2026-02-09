c0d3e2926402bf44618dcd8fee03b8970ff598b0
prometheus_jid <toozh99@gmail.com>
2026-02-09 05:28:45 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// Service Worker for Nano Banana Pro
+const CACHE_NAME = 'nano-banana-v1';
+const urlsToCache = [
+  '/',
+  '/nano-banana-pro.html',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
